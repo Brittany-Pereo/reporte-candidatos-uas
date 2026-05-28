3f0acfd04f1cbf5706c58854bf238af410d80ad9
Brittany Cecilia Pereo Sánchez <brittany.pereo@imssbienestar.gob.mx>
2026-05-27 18:22:27 -0600

    Actualizar reporte automatico

---
+++ actualizar_reporte.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd /d "C:\Users\brittany.pereo\OneDrive - IMSS-BIENESTAR\reporte-candidatos-uas"
+
+"C:\Program Files\R\R-4.5.1\bin\x64\Rscript.exe" "C:\Users\brittany.pereo\OneDrive - IMSS-BIENESTAR\reporte-candidatos-uas\Codigo para actualizar HTLM.R"
+
+exit
