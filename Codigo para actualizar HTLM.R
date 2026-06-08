032b9577b2d722245bf7b5355ad0cce41f641755
brittany-pereo_IMSSB <brittany-pereo_IMSSB@users.noreply.github.com>
2026-06-08 10:29:19 -0600

    Actualizado

--- Codigo para actualizar HTLM.R
+++ Codigo para actualizar HTLM.R
@@ -1,5 +1,9 @@
 setwd("C:/Users/brittany.pereo/OneDrive - IMSS-BIENESTAR/reporte-candidatos-uas/reporte-candidatos-uas")
 
+#Si marca error porque se boror el rmarldown correr lo siguiente en la terminal
+# git restore reporte_google_sheet.rmarkdown
+# git status
+
 system("git pull --rebase")
 
 quarto::quarto_render("reporte_google_sheet.qmd")
